2f0ff075e7297de736c0c1311c76c2ba45890191
liuyujiao <liuyujiao_love1@sina.com>
2018-10-22 15:06:07 +0800

    tmp commit

---
+++ UtilStrExample/UtilStrExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UtilStrExample
+//
+//  Created by 刘玉娇 on 2018/10/22.
+//  Copyright © 2018年 yunli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
